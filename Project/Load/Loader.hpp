5d27c7eaad2441ad25fd67e8d16f852227f4e851
halo505444-ops <halo505444@gmail.com>
2026-09-17 01:07:33 +0300

    Create Loader.hpp

---
+++ Project/Load/Loader.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#import <Foundation/Foundation.h>
+
+inline void InitializeLoader() {
+    // پشکنینی سەرەتایی بۆ دڵنیابوون لە ئامادەبوونی فەرمانەکان پێش لۆدبوون
+    NSLog(@"[MamaHala] Loader initialized successfully.");
+}
